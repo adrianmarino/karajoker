d31221613cc8f32ce426671279dfee5c790fb2f0
Adrian Norberto Marino <adrianmarino@gmail.com>
2015-03-29 16:42:30 -0300

    improve module nesting

--- spec/karajoker/youtube/youtube_searcher_spec.rb
+++ spec/karajoker/youtube/youtube_searcher_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
-module Karajoker::Youtube
-  describe KaraokeSearcher do
-    describe "#search" do
-      let(:song) { { query: "Through Her Eyes dream theater" } }
+module Karajoker
+  module Youtube
+    describe KaraokeSearcher do
+      describe "#search" do
+        let(:song) { { query: "Through Her Eyes dream theater" } }
 
-      it "found song" do
-        results = subject.search(song)
-        expect(results.first.title).to include("Through Her Eyes")
+        it "found song" do
+          results = subject.search(song)
+          expect(results.first.title).to include("Through Her Eyes")
+        end
       end
     end
   end
