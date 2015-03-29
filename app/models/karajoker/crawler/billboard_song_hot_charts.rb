057553af8cbfae4e9826601d8da62702c57f3d2a
Adrian Norberto Marino <adrianmarino@gmail.com>
2015-03-29 20:29:49 -0300

    add namespace when create a new instance of song

--- app/models/karajoker/crawler/billboard_song_hot_charts.rb
+++ app/models/karajoker/crawler/billboard_song_hot_charts.rb
@@ -1,36 +1,38 @@
 require'nokogiri'
 
-module Karajoker::Crawler
-  class BillboardSongHotCharts
+module Karajoker
+  module Crawler
+    class BillboardSongHotCharts
 
-    def songs(filters)
-      songs = items.inject([]) { |a, e| a << new_song(e) }
-      songs.take(filters[:top])
+      def songs(filters)
+        songs = items.inject([]) { |a, e| a << new_song(e) }
+        songs.take(filters[:top])
+      end
+
+      def initialize
+        @page = Nokogiri::HTML(open(URL))
+      end
+
+      private
+
+      def items
+        @page.css('div[class=row-title]')
+      end
+
+      def title(item)
+        item.children.css('h2').children.to_s.strip
+      end
+
+      def author(item)
+        item.children.css('h3').css('a').children.to_s.strip
+      end
+
+      def new_song(item)
+        Karajoker::Song.new(title(item), author(item))
+      end
+
+      URL = "http://www.billboard.com/charts/hot-100"
+
     end
-
-    def initialize
-      @page = Nokogiri::HTML(open(URL))
-    end
-
-    private
-
-    def items
-      @page.css('div[class=row-title]')
-    end
-
-    def title(item)
-      item.children.css('h2').children.to_s.strip
-    end
-
-    def author(item)
-      item.children.css('h3').css('a').children.to_s.strip
-    end
-
-    def new_song(item)
-      Song.new(title(item), author(item))
-    end
-
-    URL = "http://www.billboard.com/charts/hot-100"
-
   end
 end
